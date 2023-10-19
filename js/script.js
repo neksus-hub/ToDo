82cc31468636e9cff1baad45e71f75e094e61bf2
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-19 21:29:31 +0300

    add listener for form + add todoData array

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ const headerInput = document.querySelector(".header-input");
 const todoList = document.querySelector(".todo-list");
 const todoCompleted = document.querySelector(".todo-completed");
 
+const todoData = [
+  {
+    text: "Сварить кофе",
+    completed: false,
+  },
+  {
+    text: "Помыть посуду",
+    completed: true,
+  },
+];
+
+todoControl.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
 console.log("Hello, World!");
